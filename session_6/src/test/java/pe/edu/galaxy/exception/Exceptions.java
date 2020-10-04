cd08610bf182992f4424622244e3ffa0f4a5f9a7
Luis Ruiz <lruizajax@gmail.com>
2020-10-04 14:17:56 -0500

    adding session 6

---
+++ session_6/src/test/java/pe/edu/galaxy/exception/Exceptions.java
@@ -0,0 +1,49 @@
+package pe.edu.galaxy.exception;
+
+public class Exceptions {
+
+	public static void main(String[] args) throws Exception {
+		withOutException();
+	}
+
+	public static void dividir() {
+		System.out.println("Exception Handling");
+		int d = 1 / 0;
+		System.out.println("TEST HAS BEEN COMPLETED");
+	}
+
+	public static void withException() {
+		try {
+
+			System.out.println("Exception Handling");
+			int d = 1 / 0;
+			System.out.println("TEST HAS BEEN COMPLETED withException");
+
+		} catch (Exception e) {
+			System.out.println("Exception Bloque");
+			System.out.println("Message -->" + e.getMessage());
+			System.out.println("Cause -->" + e.getCause());
+			e.printStackTrace();
+		} finally {
+			System.out.println("Finally block");
+		}
+	}
+
+	public static void withOutException() throws Exception{
+		try {
+
+			System.out.println("Exception Handling");
+			int d = 1 / 0;
+			System.out.println("TEST HAS BEEN COMPLETED withOutException");
+
+		} catch (Exception e) {
+			System.out.println("Exception Bloque");
+			System.out.println("Message -->" + e.getMessage());
+			System.out.println("Cause -->" + e.getCause());
+			e.printStackTrace();
+		} finally {
+			System.out.println("Finally block");
+		}
+	}
+	
+}
