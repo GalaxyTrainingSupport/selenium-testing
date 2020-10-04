547b687a6e7f6ec90cd1eca3eb686cc08acabdc2
Luis Ruiz <lruizajax@gmail.com>
2020-10-04 14:38:53 -0500

    testng browser

---
+++ session_6/src/test/java/pe/edu/galaxy/properties/PropetyBrowser.java
@@ -0,0 +1,37 @@
+package pe.edu.galaxy.properties;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class PropetyBrowser {
+
+	WebDriver driver = null;
+	public static String WebBrowser = null;
+	
+	@BeforeSuite
+	public void setup() {
+		System.out.println("setup system properties");
+	}
+	
+	@BeforeTest
+	public void launchBrowser() {
+		
+		
+	}
+	
+	@Test
+  public void test() {
+		System.out.println(driver.getTitle());
+		System.out.println("Test Completed");
+		//
+  }
+	
+	
+	@BeforeClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
