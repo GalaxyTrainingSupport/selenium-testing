e98e98f143c7a26794e7b70286fffd180dca5196
Luis Ruiz <lruizajax@gmail.com>
2020-10-04 12:36:31 -0500

    add extend report html

--- session_5/src/test/java/pe/edu/galaxy/extend/HtmlExtend.java
+++ session_5/src/test/java/pe/edu/galaxy/extend/HtmlExtend.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
@@ -14,45 +15,60 @@ import pe.edu.galaxy.util.BaseTest;
 
 public class HtmlExtend extends BaseTest {
 
-	int int_random = ThreadLocalRandom.current().nextInt();
+int int_random = ThreadLocalRandom.current().nextInt();
 	
-	ExtentHtmlReporter report = new ExtentHtmlReporter("./reports/ejemplo"+ int_random +".html");
-	ExtentReports extent = new ExtentReports();
+	ExtentHtmlReporter report = new ExtentHtmlReporter("./reports/HtmlReport_"+ int_random +".html");
+	ExtentReports extend = new ExtentReports();
 	String path = System.getProperty("user.dir");
 
-	
-	
-	@Test (description = "Search on Google ...")
-	public void testGoogle() throws IOException {
-		extent.attachReporter(report);
-		ExtentTest logger = extent.createTest("Test Google!");
-		
-		/*LOGIC TEST {
-		 * 
-		 * 
-		 * 
-		 * } */
-		
+	@Test (description="Search on Peruano")
+	public void TestGoogle() throws IOException {
+		extend.attachReporter(report);
+		ExtentTest logger =  extend.createTest("El Peruano Test");
+		getDriver().get("https://elperuano.pe/");
 		logger.log(Status.PASS, getDriver().getTitle());
-		logger.addScreenCaptureFromPath(path + "/screenshot/google.png");
-		logger.assignCategory("Regression Test","End2End Test");
-		logger.assignAuthor("Milena Melo");
-		extent.flush();	
+		logger.addScreenCaptureFromPath(path + "/screenshot/google.jpg");
+		logger.assignCategory("Regrestion Test");
+		logger.assignAuthor("Luis Ruiz", "Milena Melo");
+		extend.flush();
 	}
 	
-	@Test (description = "Search on Yahoo ...")
-	public void testYahoo() throws IOException {
-		extent.attachReporter(report);
-		ExtentTest logger2 = extent.createTest("Test Yahoo?");
+	@Test (description="Breaking news")
+	public void TestYahoo() throws IOException {
+		ExtentTest logger2 =  extend.createTest("Breaking News","Verify the breaking news have been published");
+		getDriver().get("https://elcomercio.com.pe/");
+		logger2.log(Status.FAIL, getDriver().getTitle());
+		logger2.fail("because url not found on server", MediaEntityBuilder.createScreenCaptureFromPath(path + "/screenshot/errors/404-error.png").build());
+		logger2.assignCategory("End to End");
+		logger2.assignAuthor("Luis Ruiz", "Liz Ayuque");
+		extend.flush();
 		
-		/*LOGIC TEST {
-		 * 
-		 * 
-		 * 
-		 * } */
+	}
+	
+	@Test (description="Search on Selenium")
+	public void TestSelenium() throws IOException {
+		ExtentTest logger3 =  extend.createTest("Selenium Test");
+		getDriver().get("http://selenium.dev/");
+		logger3.log(Status.PASS, getDriver().getTitle());
+		logger3.pass("evidences from website responsive", MediaEntityBuilder.createScreenCaptureFromPath(path + "/screenshot/selenium.png").build());
+		logger3.assignCategory("Integration Test");
+		logger3.assignAuthor("Milena Melo", "Liz Ayuque");
+		extend.flush();
 		
-		logger2.log(Status.FAIL, "verify title");
-		logger2.addScreenCaptureFromPath("/Users/lruiz/Desktop/selenium-testng/session_5/screenshot/errors/404.png");
-		extent.flush();	
+	}
+	
+	
+	@Test
+	public void TestExtendReports() throws IOException {
+		ExtentTest parentTest = extend.createTest("MyFirstTest");
+		ExtentTest childTest = parentTest.createNode("MyFirstChildTest","Node Description");
+		childTest.pass("details");
+		
+		getDriver().get("http://www.extentreports.com/");
+		
+		parentTest.log(Status.PASS, getDriver().getTitle());
+		parentTest.assignCategory("Integration Test");
+		extend.flush();
+		
 	}
 }
