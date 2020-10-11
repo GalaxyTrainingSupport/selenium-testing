6f3c2cf577b54e3e2f82b85b14c788e58b47c65f
Luis Ruiz <lruizajax@gmail.com>
2020-10-11 13:50:51 -0500

    multi browser with json data

--- session_8/src/test/java/pe/edu/galaxy/browser/MultiBrowser.java
+++ session_8/src/test/java/pe/edu/galaxy/browser/MultiBrowser.java
@@ -1,26 +1,33 @@
 package pe.edu.galaxy.browser;
 
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
 
 public class MultiBrowser {
 	WebDriver driver = null;
 
 	@Parameters("BrowserName")
-	@BeforeTest
+	@BeforeClass
 	public void setup(String BrowserName) {
-		
-		System.out.println("Thread Id" + Thread.currentThread().getId());
 
+		System.out.println("Thread Id" + Thread.currentThread().getId());
 		if (BrowserName.equalsIgnoreCase("FIREFOX")) {
 			driver = new FirefoxDriver();
 		} else if (BrowserName.equalsIgnoreCase("chrome")) {
@@ -32,15 +39,38 @@ public class MultiBrowser {
 		}
 	}
 
+	@DataProvider(name = "dp")
+	public String[] readJson() throws IOException, ParseException {
+		JSONParser jsonParser = new JSONParser();
+		FileReader reader = new FileReader("/Users/lruiz/Desktop/selenium-testng/session_8/data/users.json");
 
-	@Test
-	public void google() throws InterruptedException {
-		driver.get("http://google.com.pe/");
-		driver.manage().window().maximize();
-		Thread.sleep(3000);
+		Object obj = jsonParser.parse(reader);
+
+		JSONObject UsersJsonObj = (JSONObject) obj;
+		JSONArray UsersjsonArr = (JSONArray) UsersJsonObj.get("users");
+
+		String datausers[] = new String[UsersjsonArr.size()];
+
+		for (int i = 0; i < UsersjsonArr.size(); i++) {
+			JSONObject users = (JSONObject) UsersjsonArr.get(i);
+			String username = (String) users.get("username");
+			String password = (String) users.get("password");
+
+			datausers[i] = username + "," + password;
+		}
+		return datausers;
 	}
 
-	@AfterMethod
+	@Test(dataProvider = "dp")
+	public void doLogin(String data) throws InterruptedException {
+		String users[] = data.split(",");
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.findElement(By.id("txtUsername")).sendKeys(users[0]);
+		driver.findElement(By.id("txtPassword")).sendKeys(users[1]);
+		driver.findElement(By.id("btnLogin")).submit();
+
+	}
+
 	@AfterSuite
 	public void tearDown() {
 		driver.quit();
