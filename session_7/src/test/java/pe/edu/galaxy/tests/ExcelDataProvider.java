4ba548872eb628de2940cefef0464f64404eae96
Luis Ruiz <lruizajax@gmail.com>
2020-10-11 12:42:29 -0500

    test with data provider

--- session_7/src/test/java/pe/edu/galaxy/tests/ExcelDataProvider.java
+++ session_7/src/test/java/pe/edu/galaxy/tests/ExcelDataProvider.java
@@ -8,63 +8,62 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import pe.edu.galaxy.objectrepositity.ExcelFile;
+import pe.edu.galaxy.objectrepositity.*;
 
 public class ExcelDataProvider {
-
+	
 	WebDriver driver = null;
-
+	
 	@BeforeTest
 	public void setupTest() {
 		driver = new ChromeDriver();
 	}
-
 	
 	@Test(dataProvider="DataLogin")
-	public void doLogin(String userName, String password ) throws InterruptedException {
-		System.out.println(userName + "--" + password );
-		
+	public void TestWithExcelData(String UserName, String Password) throws InterruptedException {
+		System.out.println(UserName + " | " + Password);
 		driver.get("https://opensource-demo.orangehrmlive.com/");
-		
-		driver.findElement(By.id("txtUsername")).sendKeys(userName);
-		driver.findElement(By.id("txtPassword")).sendKeys(password);
-		
+		driver.findElement(By.id("txtUsername")).sendKeys(UserName);
+		driver.findElement(By.id("txtPassword")).sendKeys(Password);
 		//driver.findElement(By.name("Submit"));
 		//Assert.assertEquals("Test completed", "OrangeHRM", driver.getTitle());
-
-		Thread.sleep(2000);
+		Thread.sleep(2000);	
 	}
 	
-
-	@DataProvider(name = "DataLogin")
-	  public Object[][] getData() {
-
+	@DataProvider(name="DataLogin")
+	public Object[][] getData() {
+		
 		String excelPath = "/Users/lruiz/Desktop/selenium-testng/session_7/data/data.xlsx";
-		String sheetName = "Hoja1";
-		Object data = testData(excelPath, sheetName);
-		return (Object[][]) data;	
+		Object data = (Object) testData(excelPath, "hoja1");
+	
+		return (Object[][]) data;
+		
 	}
 	
 	public Object testData(String excelPath, String sheetName) {
-		ExcelFile excel = new ExcelFile(excelPath, sheetName);
-		int RowCount = excel.getColCount();
-		int ColCount = excel.getColCount();
-
-		Object data[][] = new Object[RowCount - 1][ColCount];
-
-		for (int i = 1; i < RowCount; i++) {
-			for (int j = 0; j < ColCount; j++) {
-				String CellData = excel.getCellDataString(i, j);
-				data[i - 1][j] = CellData;
-			}
-		}
 		
+		ExcelFile excel  = new ExcelFile(excelPath, sheetName);
+		
+		int rowCount = excel.getRowCount();
+		int colCount = excel.getColCount();
+		
+		Object data[][] = new Object[rowCount-1][colCount];
+		
+		for (int i = 1; i < rowCount; i++) {
+			for (int j = 0; j < colCount; j++) {
+				
+				String CellData = excel.getCellDataString(i,j);
+				System.out.println(CellData + " | ");
+				data[i-1][j] = CellData;
+				
+			}
+			System.out.println();
+		}
 		return data;
-
 	}
-	
+
 	@AfterSuite
-	public void tearDonw() {
+	public void tearDown() {
 		driver.quit();
 	}
 }
