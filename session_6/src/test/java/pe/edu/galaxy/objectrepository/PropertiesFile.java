75a9a0a76d70f95e03665a321d66a91decdaf0a9
Luis Ruiz <lruizajax@gmail.com>
2020-10-11 09:40:17 -0500

    add setproperties

--- session_6/src/test/java/pe/edu/galaxy/objectrepository/PropertiesFile.java
+++ session_6/src/test/java/pe/edu/galaxy/objectrepository/PropertiesFile.java
@@ -1,38 +1,47 @@
 package pe.edu.galaxy.objectrepository;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesFile {
-	
+
 	static Properties prop = new Properties();
-	static String path  = System.getProperty("user.dir");
-	
+	static String path = System.getProperty("user.dir");
+
 	public static void main(String[] args) {
 		getProperties();
+		setProperties();
 
 	}
 
 	public static void getProperties() {
-		
+
 		try {
-			///Users/lruiz/Desktop/selenium-testng/session_6
-			InputStream input = new FileInputStream("/Users/lruiz/Desktop/selenium-testng/session_6/data/application.properties");
+			FileInputStream input = new FileInputStream(path + "/data/application.properties");
 			prop.load(input);
-			System.out.println("Browser -->"+ prop.getProperty("browser"));
-			
+			System.out.println("Browser -->" + prop.getProperty("browser"));
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			System.out.println();
 			e.printStackTrace();
 		}
-		
-		
-		
+
 	}
 
-public static void setProperties() {
-		
+	public static void setProperties() {
+		try {
+			FileOutputStream output = new FileOutputStream(path + "/data/application.properties");
+			prop.setProperty("result", "Past Test");
+			prop.store(output, null);
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println();
+			e.printStackTrace();
+		}
+
 	}
 }
