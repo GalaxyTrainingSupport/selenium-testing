1f21b3039159406fd40661f9079c3e7e96fc3200
Luis Ruiz <lruizajax@gmail.com>
2020-10-11 11:33:18 -0500

    getCellDataNumber

--- session_7/src/test/java/pe/edu/galaxy/objectrepositity/ExcelFile.java
+++ session_7/src/test/java/pe/edu/galaxy/objectrepositity/ExcelFile.java
@@ -11,20 +11,68 @@ public class ExcelFile {
 
 	public static void main(String[] args) {
 		getRowCount();
+		getColCount();
+		getCellDataNumber(1,2);
 	}
 
 	public static int getRowCount() {
 		int rowCount = 0;
 		try {
-			workbook = new XSSFWorkbook(path + "/data/excel/data.xlsx");
+			workbook = new XSSFWorkbook("/Users/lruiz/Desktop/selenium-testng/session_7/data/data.xlsx");
 			sheet = workbook.getSheet("Hoja1");
 			rowCount = sheet.getPhysicalNumberOfRows();
-			System.out.println("Number of Rows" + rowCount);
+			System.out.println("Number of Rows -->" + rowCount);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			System.out.println();
 			e.printStackTrace();
 		}
 		return rowCount;
+	}
+	
+	public static int getColCount() {
+		int colCount = 0;
+		try {
+			workbook = new XSSFWorkbook("/Users/lruiz/Desktop/selenium-testng/session_7/data/data.xlsx");
+			sheet = workbook.getSheet("Hoja1");
+			colCount = sheet.getRow(0).getPhysicalNumberOfCells();
+			System.out.println("Number of Cols -->" + colCount);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println();
+			e.printStackTrace();
+		}
+		return colCount;
+	}
+	
+	public static double getCellDataNumber(int rownum, int colnum) {
+		double cellData=0;
+		try {
+		workbook = new XSSFWorkbook(path + "/data/data.xlsx");
+		sheet = workbook.getSheet("Hoja1");
+		cellData = sheet.getRow(rownum).getCell(colnum).getNumericCellValue();
+		System.out.println("Number double Rows: " + cellData);
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println();
+			e.printStackTrace();
+		}
+		return cellData;
+	}
+	
+	public static String getCellDataString(int rownum, int colnum) {
+		String cellData = null;
+		try {
+		workbook = new XSSFWorkbook(path + "/data/data.xlsx");
+		sheet = workbook.getSheet("Hoja1");
+		cellData = sheet.getRow(rownum).getCell(colnum).getStringCellValue();
+		System.out.println("Number String Rows: " + cellData);
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println();
+			e.printStackTrace();
+		}
+		return cellData;
 	}
 }
