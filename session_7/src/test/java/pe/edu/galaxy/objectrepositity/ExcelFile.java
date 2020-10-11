406220853e2493525f96a8a91bb4e2c208589045
Luis Ruiz <lruizajax@gmail.com>
2020-10-11 11:23:36 -0500

    getRowCount

--- session_7/src/test/java/pe/edu/galaxy/objectrepositity/ExcelFile.java
+++ session_7/src/test/java/pe/edu/galaxy/objectrepositity/ExcelFile.java
@@ -1,9 +1,30 @@
 package pe.edu.galaxy.objectrepositity;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelFile {
-	
+
+	public static String path = System.getProperty("user.dir");
+	public static XSSFWorkbook workbook;
+	public static XSSFSheet sheet;
+
 	public static void main(String[] args) {
-		
+		getRowCount();
 	}
 
+	public static int getRowCount() {
+		int rowCount = 0;
+		try {
+			workbook = new XSSFWorkbook(path + "/data/excel/data.xlsx");
+			sheet = workbook.getSheet("Hoja1");
+			rowCount = sheet.getPhysicalNumberOfRows();
+			System.out.println("Number of Rows" + rowCount);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println();
+			e.printStackTrace();
+		}
+		return rowCount;
+	}
 }
